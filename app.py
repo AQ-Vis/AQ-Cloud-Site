dcc26a83faaa3871be510dcfa16c15003e6ac6b9
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-26 16:25:14 +0530

    Added a simple json parser

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
 
@@ -11,3 +11,12 @@ def root():
 @app.route('/homepage')
 def homepage():
 	return render_template('index.html')
+
+#Basic JSON input
+@app.route('/api/input_data', methods=['POST'])
+def input_data():
+	inputData = dict(request.json)
+	device_id = str(inputData['device_id'])
+	ppm = int(inputData['ppm'])
+	print(ppm,device_id)
+	return Response(status=200)
