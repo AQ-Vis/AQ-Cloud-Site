67fa18f53010a81bcc90c887ec1333bbd1db5d7c
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-06-01 15:52:08 +0530

    Cleanup

--- static/js/chartloader.js
+++ static/js/chartloader.js
@@ -6,38 +6,35 @@ $.ajax({
   async: true,
   statusCode: {
     200: function(msg) {
-      console.log("Success");
-      console.log(msg);
+      //Getting the data
       var datapoints = msg.data;
       console.log(datapoints);
+      //parsing the data
       var dataparsed = [];
-
       datapoints.forEach((item, i) => {
         dataparsed.push(item['ppm'])
       });
-
+      //charting the data
       var ctx = document.getElementById('myChart').getContext('2d');
       var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: [1,2,3,4,5],
-    datasets: [{
-        data: dataparsed,
-        label: "PPM device 1",
-        borderColor: "#3e95cd",
-        fill: false
-      }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'PPM'
-    },
-    responsive: false
-  }
-});
-
-
+        type: 'line',
+        data: {
+          labels: [1,2,3,4,5],
+          datasets: [{
+            data: dataparsed,
+            label: "PPM device 1",
+            borderColor: "#3e95cd",
+            fill: false
+          }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'PPM'
+          },
+          responsive: false
+        }
+      });
     },
     500: function(msq) {
       console.log("Internal Server Error");
